5546a41c0b1819f93c7a9ffe9d0b52ef39dd1206
Matias Hidalgo <mrhidalgo@leadtech.com>
2025-03-02 23:26:08 +0100

    Refactor 2

--- src/main/java/com/agiletv/user/context/infrastructure/mapper/LocationMapper.java
+++
@@ -1,8 +0,0 @@
-package com.agiletv.user.context.infrastructure.mapper;
-
-import com.agiletv.user.context.infrastructure.domain.LocationEntity;
-import com.agiletv.user.context.model.dto.LocationDto;
-import org.mapstruct.Mapper;
-
-@Mapper(componentModel = "spring", config = GenericMapper.class)
-public interface LocationMapper extends GenericMapper<LocationDto, LocationEntity>  { }
--- src/main/java/com/agiletv/user/context/infrastructure/mapper/UserMapper.java
+++
@@ -1,8 +0,0 @@
-package com.agiletv.user.context.infrastructure.mapper;
-
-import com.agiletv.user.context.infrastructure.domain.UserEntity;
-import com.agiletv.user.context.model.dto.UserDto;
-import org.mapstruct.Mapper;
-
-@Mapper(componentModel = "spring", config = GenericMapper.class)
-public interface UserMapper extends GenericMapper<UserDto, UserEntity>  { }
